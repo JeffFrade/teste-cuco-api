5a715b05d6f1f2a7166a37e8549b87c9ff954a61
Jefferson Frade <jefferrson.frade@gmail.com>
2020-12-16 19:22:36 -0300

    Fixes on PHP Dockerfile to add pdo_mysql extension

--- app/Repositories/Models/Client.php
+++ app/Repositories/Models/Client.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class Client extends Model
 {
     use SoftDeletes;
+
+    protected $fillable = [
+        'name',
+        'birth_date',
+        'phone'
+    ];
 }
