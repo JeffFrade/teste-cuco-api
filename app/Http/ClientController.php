c6ae26f344aab0d078f963ccdd6eb738a4f25e53
Jefferson Frade <jefferrson.frade@gmail.com>
2020-12-16 19:41:00 -0300

    Get all clients

--- app/Http/ClientController.php
+++ app/Http/ClientController.php
@@ -3,8 +3,38 @@
 namespace App\Http;
 
 use App\Core\Support\Controller;
+use App\Service\Client;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
 
 class ClientController extends Controller
 {
+    /**
+     * @var Client
+     */
+    private $client;
 
+    /**
+     * ClientController constructor.
+     * @param Client $client
+     */
+    public function __construct(Client $client)
+    {
+        $this->client = $client;
+    }
+
+    /**
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function index(Request $request)
+    {
+         $data = $this->client->index($request->all() ?? []);
+         $response = [
+             'code' => 1,
+             'data' => $data
+         ];
+
+         return response()->json($response);
+    }
 }
--- app/Service/Client.php
+++ app/Service/Client.php
@@ -1,10 +1,42 @@
 <?php
 
-
 namespace App\Service;
 
+use App\Repositories\ClientRepository;
 
 class Client
 {
+    /**
+     * @var ClientRepository
+     */
+    private $clientRepository;
 
+    /**
+     * @param ClientRepository $clientRepository
+     */
+    public function setClientRepository(ClientRepository $clientRepository)
+    {
+        $this->clientRepository = $clientRepository;
+    }
+
+    /**
+     * @return ClientRepository
+     */
+    public function getClientRepository()
+    {
+        return $this->clientRepository ?? $this->createDefaultClientRepository();
+    }
+
+    /**
+     * @return ClientRepository
+     */
+    private function createDefaultClientRepository()
+    {
+        return new ClientRepository();
+    }
+
+    public function index(array $params)
+    {
+        return $this->getClientRepository()->allNoTrashed();
+    }
 }
